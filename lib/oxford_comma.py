3b0de16527ab1289e7d26c416875ea836a72259f
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-09-02 20:26:59 +0300

    added

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(arr):
+    # lib/oxford_comma.py
+
+   
+    if len(arr) == 0:
+        return ""
+    elif len(arr) == 1:
+        return arr[0]
+    elif len(arr) == 2:
+        return f"{arr[0]} and {arr[1]}"
+    else:
+        c_separated = ", ".join(arr[:-1])
+        return f"{c_separated}, and {arr[-1]}"
